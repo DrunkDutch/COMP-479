ccf5c3d031bcd6aa99b2897172f18a0a58a0d724
Devin Mens <devin.mens@gmail.com>
2017-09-21 15:08:51 -0400

    Reworked tokenizer to include text from relevant metadata

--- comp479-core/core.py
+++ comp479-core/core.py
@@ -2,6 +2,7 @@ import numpy
 import nltk
 import os
 import re
+import collections
 
 # TODO: Look into wrapping the tag parser into a helper function for refined parsing
 # TODO: Create wrapper class to hold the parsed text from above function
@@ -14,14 +15,18 @@ for file in os.listdir("./../Corpus"):
 
 with open(corpus[0], 'r') as myfile:
     data = myfile.read()
+
+
 class Document:
+
     def __init__(self, source):
         self.raw = self.parse_tags("REUTERS", source, False)[0]
-        self.topics = self.parse_tags("D", self.raw)
+        self.topics_places = self.parse_tags("D", self.raw)
         self.text = self.parse_tags("TEXT", self.raw)[0]
         self.title = self.parse_tags("TITLE", self.text)[0]
         self.dateline = self.parse_tags("DATELINE", self.text)[0]
         self.body = self.get_body()
+        self.tokens, self.occurence = self.tokenize()
 
     """
     Parser function to find the information contained within the desired tags
@@ -46,6 +51,22 @@ class Document:
         else:
             return self.text.split("</DATELINE>")[1].split("</TEXT>")[0]
 
+    def tokenize(self):
+        token_list = []
+        stemmer = nltk.PorterStemmer()
+        count_list = {}
+        text = [self.body, self.title]
+        text.extend(self.topics_places)
+        for section in text:
+            for word in nltk.word_tokenize(section):
+                stemmed_word = stemmer.stem(word)
+                if stemmed_word in count_list.keys():
+                    count_list[stemmed_word] = count_list[stemmed_word] + 1
+                else:
+                    count_list[stemmed_word] = 1
+                token_list.append(stemmed_word)
+        return set(token_list), collections.OrderedDict(sorted(count_list.items()))
+
 
 
 first_pass = Document(data)
@@ -54,4 +75,5 @@ Install punkt package from nltk to be able to tokenize english
 """
 
 
-print nltk.sent_tokenize(first_pass.body)
+print first_pass.title
+print first_pass.occurence
