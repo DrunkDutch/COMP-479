e3cd4ec09520719567f52706c8ae591dab4fe854
Devin Mens <devin.mens@gmail.com>
2017-09-26 15:42:35 -0400

    Began sorting the postings list for each document and added docuID attribute

--- comp479-core/core.py
+++ comp479-core/core.py
@@ -3,6 +3,8 @@ import nltk
 import os
 import re
 import collections
+import string
+
 
 # TODO: Look into wrapping the tag parser into a helper function for refined parsing
 # TODO: Create wrapper class to hold the parsed text from above function
@@ -21,6 +23,7 @@ class Document:
 
     def __init__(self, source):
         self.raw = self.parse_tags("REUTERS", source, False)[0]
+        self.id = self.get_id()
         self.topics_places = self.parse_tags("D", self.raw)
         self.text = self.parse_tags("TEXT", self.raw)[0]
         self.title = self.parse_tags("TITLE", self.text)[0]
@@ -42,6 +45,12 @@ class Document:
         results = re.findall(body_regex, source)
         return results
 
+    def get_id(self):
+        regex = re.compile("(?<=NEWID=\")\d+")
+        result = re.findall(regex, self.raw)
+        return result[0]
+
+
     """
     Parser function to return the actual body of the text, stripping away all metadata and tags
     """
@@ -51,8 +60,18 @@ class Document:
         else:
             return self.text.split("</DATELINE>")[1].split("</TEXT>")[0]
 
+    def cleanup(self, input):
+        dicts = {}
+        punction = [unicode(x, "utf-8") for x in string.punctuation]
+        stops = set(nltk.corpus.stopwords.words('english') + punction)
+        print stops
+        for key in input.keys():
+            if key not in stops:
+                dicts[key] = self.id
+        return dicts
+
     def tokenize(self):
-        token_list = []
+        token_list = {}
         stemmer = nltk.PorterStemmer()
         count_list = {}
         text = [self.body, self.title]
@@ -60,20 +79,25 @@ class Document:
         for section in text:
             for word in nltk.word_tokenize(section):
                 stemmed_word = stemmer.stem(word)
+                if not isinstance(stemmed_word, unicode):
+                    stemmed_word = unicode(stemmed_word, "utf-8")
                 if stemmed_word in count_list.keys():
                     count_list[stemmed_word] = count_list[stemmed_word] + 1
                 else:
                     count_list[stemmed_word] = 1
-                token_list.append(stemmed_word)
-        return set(token_list), collections.OrderedDict(sorted(count_list.items()))
-
+                if stemmed_word not in token_list:
+                    token_list[stemmed_word] = self.id
+        cleaned = self.cleanup(token_list)
+        return sorted(cleaned.items()), collections.OrderedDict(sorted(count_list.items()))
 
 
 first_pass = Document(data)
 """
 Install punkt package from nltk to be able to tokenize english
+install stopwords corpus for nltk to remove stopwords
 """
 
 
 print first_pass.title
-print first_pass.occurence
+print first_pass.id
+print first_pass.tokens
